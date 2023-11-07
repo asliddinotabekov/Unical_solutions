e17f1193d9d6beb9158b36987938a06750591fc1
Asliddin Otabekov <126315706+asliddinotabekov@users.noreply.github.com>
2023-11-07 06:05:50 +0500

    fully solution

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+
+
+// "Unical Solutions" 
+// [,],{,},(,) belgilardan iborat 
+// start berilgan. 
+// satr yaroqli ekanini tekshiring. Satr yaroqli deyiladi:
+
+//    1. Ochiq qavslar bir xil turdagi qavslar bilan yopilishi kerak.
+//    2. Ochiq qavslar to‘g‘ri tartibda yopilishi kerak.
+//    3. Har bir yopiq qavsda bir xil turdagi mos keladigan ochiq qavs bo‘lishi kerak.
+
+// Kiruvchi parametrlar
+// Birinchi qatorda 
+// satr. Uzunligi 
+// dan oshmaydi. Vaqt limiti 1 sekund.
+
+// Chiquvchi parametrlar
+// Agar qavslar to‘g‘ri ochib yopilgan bo‘lsa YES, aks holda NO so‘zlarini chiqaring.
+
+// Na'munalar
+// #	Kirish	Chiqish
+// 1	()	YES
+// 2	()[]{}	YES
+// 3	(]	NO
+
+
 function areBracketsBalanced(expr) {
     console.log()
     let stack = [];
@@ -42,7 +68,7 @@ function areBracketsBalanced(expr) {
     }
 }
 
-let expr = "()";
+let expr = prompt("Satr yaroqli !!!!!")
 
 if (areBracketsBalanced(expr))
     document.write("Yes");
